9963c424034f61842b69f5086ef1685e20aa1f25
lytrihangit <kaized123@gmail.com>
2020-10-26 20:25:57 +0700

    cc

--- core/auth/login.php
+++ core/auth/login.php
@@ -1,3 +1,41 @@
 <?php 
     header('content-type: application/json');
     include("../../system/init.php");
+
+    if ($_SESSION['csrf_key'] != $post_['csrf']){
+		exit("cut me may di!");
+    }
+    
+    if (empty($post_['username']) || empty($post_['password'])) {
+        exit(json_encode(array(
+            "status" => 0,
+            "msg" => "Vui Lòng Nhập Tài Khoản Hoặc Mật Khẩu !"
+        )));
+    }
+
+    $username = $post_['username'];
+    $password = $post_['password'];
+
+    $accounts = $conn->query("SELECT * FROM `users` WHERE `name` = '{$username}' OR `email` = '{$username}'");
+
+    if (!$accounts->num_rows > 0) {
+        exit(json_encode(array(
+            "status" => 0,
+            "msg" => "Tài Khoản Không Tồn Tại !"
+        )));
+    }
+
+    $accounts = $accounts->fetch_array();
+
+    if ($password !== $accounts['password']) {
+        exit(json_encode(array(
+            "status" => 0,
+            "msg" => "Sai Mật Khẩu !"
+        )));
+    }
+
+    $_SESSION['login'] = $username;
+    exit(json_encode(array(
+        "status" => 200,
+        "msg" => "Đăng Nhập Thành Công !"
+    )));
--- core/auth/register.php
+++ core/auth/register.php
@@ -1,3 +1,41 @@
 <?php 
     header('content-type: application/json');
     include("../../system/init.php");
+
+    if ($_SESSION['csrf_key'] != $post_['csrf']){
+		exit("cut me may di!");
+    }
+    
+    if (empty($post_['username']) || empty($post_['email']) || empty($post_['password']) || empty($post_['repassword'])) {
+        exit(json_encode(array(
+            "status" => 0,
+            "msg" => "Vui Lòng Nhập Đầy Đủ Thông Tin !"
+        )));
+    }
+
+    $username = $post_['username'];
+    $email = $post_['password'];
+    $password = $post_['password'];
+    $repassword = $post_['repassword'];
+
+    $accounts = $conn->query("SELECT * FROM `users` WHERE `name` = '{$username}' OR `email` = '{$username}'");
+
+    if ($accounts->num_rows > 0) {
+        exit(json_encode(array(
+            "status" => 0,
+            "msg" => "Tài Khoản Đã Tồn Tại !"
+        )));
+    }
+
+    if ($password !== repassword) {
+        exit(json_encode(array(
+            "status" => 0,
+            "msg" => "Mật Khẩu Nhập Lại Không Khớp !"
+        )));
+    }
+
+    $_SESSION['login'] = $username;
+    exit(json_encode(array(
+        "status" => 200,
+        "msg" => "Đăng Nhập Thành Công !"
+    )));
